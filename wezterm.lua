de086ed98bf71bab984ed8874172735b7c34ef39
ranger097 <mrgentry13@gmail.com>
2025-10-04 00:25:53 -0400

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,15 +1,16 @@
+
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 config.colors = {
-foreground = '#d1dde0',
-background = '#06080F',
+foreground = '#ebdbb2',
+background = '#1d2021',
 cursor_bg = '#3C6085',
 cursor_fg = '#62728A',
 cursor_border = '#6B91A0',
 selection_fg = '#78A6C7',
-selection_bg = '#949AAA',
+selection_bg = '#1d2021',
 scrollbar_thumb = '#A7B7C6',
 
 }
@@ -34,10 +35,9 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 14
-config.font = wezterm.font("SpaceMono Nerd Font Mono", {weight = "Regular",style = "Normal"})
+--config.font = wezterm.font("SpaceMono Nerd Font Mono", {weight = "Regular",style = "Normal"})
 
 
 
 -- Finally, return the configuration to wezterm:
 return config
-
