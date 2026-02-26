ea5bb1866d763825e11a606ccbc720399081107d
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-02-26 18:50:28 -0500

    update configs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,13 +2,25 @@
 description = "ranger097 System Clone Flake";
 inputs = {
 nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+home-manager = {
+url = "github:nix-community/home-manager";
+inputs.nixpkgs.follows = "nixpkgs";
 };
 
-outputs = {self, nixpkgs, ... }: {
+};
+
+outputs = {self, nixpkgs, home-manager, ... }: {
 nixosConfigurations.pokemon = nixpkgs.lib.nixosSystem {
 system = "x86_64-linux";
 modules = [
 ./configuration.nix
+home-manager.nixosModules.home-manager
+{
+home-manager.useGlobalPkgs = true;
+home-manager.useUserPackages = true;
+home-manager.users.ranger = import ./home.nix;
+}
 ];
 };
 };
---
+++ nixos/home.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, lib, ... }: 
+
+let 
+repoPath = "/home/ranger/Github/ranger097s_dotfiles";
+in
+{
+home.username = "ranger";
+home.homeDirectory = "/home/ranger";
+home.stateVersion = "25.11";
+
+xdg.configFile = {
+"ghostty".source = config.lib.file.mkOutOfStoreSymlink "${repoPath}/ghostty";
+"hypr".source = config.lib.file.mkOutOfStoreSymlink "${repoPath}/hypr";
+"waybar".source = config.lib.file.mkOutOfStoreSymlink "${repoPath}/waybar";
+"wlogout".source = config.lib.file.mkOutOfStoreSymlink "${repoPath}/wlogout";
+"dolphin".source = config.lib.file.mkOutOfStoreSymlink "${repoPath}/dolphin";
+"starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${repoPath}/starship/starship.toml";
+"Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${repoPath}/vscode/settings.json";
+};
+
+home.file = {
+".distroboxrc".source = config.lib.file.mkOutOfStoreSymlink "${repoPath}/distrobox/.distroboxrc";
+};
+
+programs.bash = {
+enable = true;
+enableCompletion = true;
+bashrcExtra = ''
+source ${repoPath}/shell/bash.sh
+'';
+};
+
+home.shellAliases = {
+nixpush = "cd ${repoPath} && git add . && git commit -m 'update configs' && git push origin main";
+nixswitch = "sudo nixos-rebuild switch --flake ${repoPath}/nixos#pokemon"; 
+};
+}
+
+
